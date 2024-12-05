4904e3325ce497cce37ba9be5e02b2de1a7dc482
estefanoquiriconi <estefanoquiriconi@gmail.com>
2024-12-05 00:14:06 -0300

    typescript

---
+++ 03-typescript/src/data/heros.ts
@@ -0,0 +1,30 @@
+export interface Hero {
+    id: number;
+    name: string;
+    owner: string;
+}
+
+export const heros: Hero[] = [
+    {
+        id: 1,
+        name: "Batman",
+        owner: "DC"
+    },
+    {
+        id: 2,
+        name: "Superman",
+        owner: "DC"
+    },
+    {
+        id: 3,
+        name: "Flash",
+        owner: "DC"
+    },
+    {
+        id: 4,      
+        name: "Spiderman",
+        owner: "Marvel"
+    }
+];
+
+
